8d3807f52c99e31f3be32f938b4047c8d96302e4
buffer404 <lsv141530@gmail.com>
2023-07-12 12:11:30 +0300

    edit test

--- tb/top_tb.v
+++ tb/top_tb.v
@@ -79,7 +79,7 @@ module top_tb;
 
     task test1();
         begin
-            $display("TEST : Parallel transmission of a single packet to all output ports");
+            $display("\nTEST : Parallel transmission of a single packet to all output ports");
 
             s_data_i <= 40'b11111111_00000000_10101010_00000000_11110000;
             s_dest_i <= 10'b10_00_01_00_00;
@@ -87,11 +87,10 @@ module top_tb;
             s_valid_i <= 5'b10101;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
-            $display("m_data_o = %b   m_id_o = %b   m_last_o = %b   m_valid_o = %b   s_ready_o = %b", m_data_o, m_id_o, m_last_o, m_valid_o, s_ready_o);
             if(m_data_o == 24'b11111111_10101010_11110000 && m_id_o == 9'b100_010_000 && m_last_o == 3'b111 && m_valid_o == 3'b111 && s_ready_o == 5'b10101) begin
-                $display("Test : Correct \n");
+                $display("STEP 1 : Correct \n\n");
             end
-            else $display("Test : Failed");
+            else $display("STEP 1 : Failed\n\n");
         end
     endtask
 
@@ -105,11 +104,20 @@ module top_tb;
             s_valid_i <= 5'b10101;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
+            if(m_data_o == 24'b11110000_11110000_11110000 && m_id_o == 9'b100_010_000 && m_last_o == 3'b001 && m_valid_o == 3'b111 && s_ready_o == 5'b10101) begin
+                $display("STEP 1 : Correct");
+            end
+            else $display("STEP 1 : Failed");
+
             s_data_i <= 40'b10101010_00000000_10101010_00000000_10101010;
             s_dest_i <= 10'b10_00_01_00_00;
             s_last_i <= 5'b00100;
             s_valid_i <= 5'b10100;
             m_ready_i <= 3'b110;
+            if(m_data_o == 24'b11110000_11110000_11110000 && m_id_o == 9'b100_010_000 && m_last_o == 3'b001 && m_valid_o == 3'b111 && s_ready_o == 5'b10101) begin
+                $display("STEP 2 : Correct");
+            end
+            else $display("STEP 2 : Failed");
             #HALF_PERIOD
             s_data_i <= 40'b11111111_00000000_11111111_00000000_11111111;
             s_dest_i <= 10'b10_00_01_00_00;
@@ -117,11 +125,10 @@ module top_tb;
             s_valid_i <= 5'b10000;
             m_ready_i <= 3'b100;
             #HALF_PERIOD
-            $display("m_data_o = %b   m_id_o = %b   m_last_o = %b   m_valid_o = %b   s_ready_o = %b", m_data_o, m_id_o, m_last_o, m_valid_o, s_ready_o);
             if(m_data_o == 24'b11111111_10101010_11110000 && m_id_o == 9'b100_011_001 && m_last_o == 3'b100 && m_valid_o == 3'b100 && s_ready_o == 5'b10000) begin
-                $display("Test : Correct \n");
+                $display("STEP 3 : Correct \n\n");
             end
-            else $display("Test : Failed");
+            else $display("STEP 3 : Failed\n\n");
         end
     endtask
 
@@ -135,35 +142,50 @@ module top_tb;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
+            if(m_data_o == 24'b00000000_00000000_00000000 && m_id_o == 9'b000_000_000 && m_last_o == 3'b001 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
+                $display("STEP 1 : Correct");
+            end
+            else $display("STEP 1 : Failed");
             s_data_i <= 40'b11111111_10101010_11110000_00001111_00000000;
             s_dest_i <= 10'b00_00_00_00_00;
             s_last_i <= 5'b00010;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
+            if(m_data_o == 24'b00000000_00000000_00001111 && m_id_o == 9'b000_000_001 && m_last_o == 3'b001 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
+                $display("STEP 2 : Correct");
+            end
+            else $display("STEP 2 : Failed");            
             s_data_i <= 40'b11111111_10101010_11110000_00001111_00000000;
             s_dest_i <= 10'b00_00_00_00_00;
             s_last_i <= 5'b00100;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
+            if(m_data_o == 24'b00000000_00000000_11110000 && m_id_o == 9'b000_000_010 && m_last_o == 3'b001 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
+                $display("STEP 3 : Correct");
+            end
+            else $display("STEP 3 : Failed");            
             s_data_i <= 40'b11111111_10101010_11110000_00001111_00000000;
             s_dest_i <= 10'b00_00_00_00_00;
             s_last_i <= 5'b01000;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
+            if(m_data_o == 24'b00000000_00000000_10101010 && m_id_o == 9'b000_000_011 && m_last_o == 3'b001 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
+                $display("STEP 4 : Correct");
+            end
+            else $display("STEP 4 : Failed");            
             s_data_i <= 40'b11111111_10101010_11110000_00001111_00000000;
             s_dest_i <= 10'b00_00_00_00_00;
             s_last_i <= 5'b10000;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
-            $display("m_data_o = %b   m_id_o = %b   m_last_o = %b   m_valid_o = %b   s_ready_o = %b", m_data_o, m_id_o, m_last_o, m_valid_o, s_ready_o);
             if(m_data_o === 24'b00000000_00000000_11111111 && m_id_o == 9'b000000100 && m_last_o == 3'b001 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
-                $display("Test : Correct \n");
+                $display("STEP 5 : Correct\n\n");
             end
-            else $display("Test : Failed");            
+            else $display("STEP 5 : Failed\n\n");            
         end
     endtask
 
@@ -177,29 +199,41 @@ module top_tb;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
+            if(m_data_o == 24'b10101010_00001111_00000000 && m_id_o == 9'b011_001_000 && m_last_o == 3'b000 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
+                $display("STEP 1 : Correct");
+            end
+            else $display("STEP 1 : Failed");
             s_data_i <= 40'b11111111_10101010_11110000_00001111_00000000;
             s_dest_i <= 10'b10_10_01_01_00;
             s_last_i <= 5'b01011;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
+            if(m_data_o == 24'b10101010_00001111_00000000 && m_id_o == 9'b011_001_000 && m_last_o == 3'b111 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
+                $display("STEP 2 : Correct");
+            end
+            else $display("STEP 2 : Failed");
             s_data_i <= 40'b11111111_10101010_11110000_00001111_00000000;
             s_dest_i <= 10'b10_10_01_01_01;
             s_last_i <= 5'b10100;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
+            if(m_data_o == 24'b11111111_11110000_00000000 && m_id_o == 9'b100_010_001 && m_last_o == 3'b110 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
+                $display("STEP 3 : Correct");
+            end
+            else $display("STEP 3 : Failed");
             s_data_i <= 40'b11111111_10101010_11110000_00001111_00000000;
             s_dest_i <= 10'b10_10_01_01_01;
             s_last_i <= 5'b00001;
             s_valid_i <= 5'b11111;
             m_ready_i <= 3'b111;
             #HALF_PERIOD
-            $display("m_data_o = %b   m_id_o = %b   m_last_o = %b   m_valid_o = %b   s_ready_o = %b", m_data_o, m_id_o, m_last_o, m_valid_o, s_ready_o);
+            // $display("m_data_o = %b   m_id_o = %b   m_last_o = %b   m_valid_o = %b   s_ready_o = %b", m_data_o, m_id_o, m_last_o, m_valid_o, s_ready_o);
             if(m_data_o === 24'b10101010_00000000_00000000 && m_id_o == 9'b011_000_001 && m_last_o == 3'b010 && m_valid_o == 3'b111 && s_ready_o == 5'b11111) begin
-                $display("Test : Correct \n");
+                $display("STEP 4 : Correct \n");
             end
-            else $display("Test : Failed");            
+            else $display("STEP 4 : Failed");            
         end
     endtask
 
